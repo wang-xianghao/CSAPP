852b894f5b9112ce0a89367be834508ad447d276
Xianghao Wang <wang.xianghao@outlook.com>
2025-04-19 01:31:04 +1000

    answer.md

--- hw5/inner.c
+++ hw5/inner.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <x86intrin.h>
 
-typedef long data_t;
+typedef double data_t;
 
 typedef struct
 {
@@ -49,9 +49,9 @@ void inner1(vec_ptr u, vec_ptr v, data_t *dest) {
     long length = vec_length(u);
     data_t *u_data = u->data;
     data_t *v_data = v->data;
-    data_t sum = (data_t) 0;
+    data_t sum = (data_t)0;
 
-    for (i = 0; i < length; ++ i) {
+    for (i = 0; i < length; ++i) {
         sum = sum + u_data[i] * v_data[i];
     }
     *dest = sum;
@@ -62,18 +62,65 @@ void inner2(vec_ptr u, vec_ptr v, data_t *dest) {
     long length = vec_length(u);
     data_t *u_data = u->data;
     data_t *v_data = v->data;
-    data_t sum = (data_t) 0;
+    data_t sum = (data_t)0;
 
     for (i = 0; i < length - 5; i += 5) {
         sum = sum + u_data[i] * v_data[i];
-        sum = sum + u_data[i+1] * v_data[i+1];
-        sum = sum + u_data[i+2] * v_data[i+2];
-        sum = sum + u_data[i+3] * v_data[i+3];
-        sum = sum + u_data[i+4] * v_data[i+4];
-        sum = sum + u_data[i+5] * v_data[i+5];
+        sum = sum + u_data[i + 1] * v_data[i + 1];
+        sum = sum + u_data[i + 2] * v_data[i + 2];
+        sum = sum + u_data[i + 3] * v_data[i + 3];
+        sum = sum + u_data[i + 4] * v_data[i + 4];
+        sum = sum + u_data[i + 5] * v_data[i + 5];
     }
 
-    for (; i < length; ++ i) {
+    for (; i < length; ++i) {
+        sum = sum + u_data[i] * v_data[i];
+    }
+
+    *dest = sum;
+}
+
+void inner3(vec_ptr u, vec_ptr v, data_t *dest) {
+    long i;
+    long length = vec_length(u);
+    data_t *u_data = u->data;
+    data_t *v_data = v->data;
+    data_t sum0 = (data_t)0;
+    data_t sum1 = (data_t)0;
+    data_t sum2 = (data_t)0;
+    data_t sum3 = (data_t)0;
+    data_t sum4 = (data_t)0;
+    data_t sum5 = (data_t)0;
+
+    for (i = 0; i < length - 5; i += 5) {
+        sum0 = sum0 + u_data[i] * v_data[i];
+        sum1 = sum1 + u_data[i + 1] * v_data[i + 1];
+        sum2 = sum2 + u_data[i + 2] * v_data[i + 2];
+        sum3 = sum3 + u_data[i + 3] * v_data[i + 3];
+        sum4 = sum4 + u_data[i + 4] * v_data[i + 4];
+        sum5 = sum5 + u_data[i + 5] * v_data[i + 5];
+    }
+
+    for (; i < length; ++i) {
+        sum1 = sum1 + u_data[i] * v_data[i];
+    }
+
+    *dest = sum0 + sum1 + sum2 + sum3 + sum4 + sum5;
+}
+
+void inner4(vec_ptr u, vec_ptr v, data_t *dest) {
+    long i;
+    long length = vec_length(u);
+    data_t *u_data = u->data;
+    data_t *v_data = v->data;
+    data_t sum = (data_t)0;
+
+    for (i = 0; i < length - 5; i += 5) {
+        sum += u_data[i] * v_data[i] + u_data[i + 1] * v_data[i + 1] + u_data[i + 2] * v_data[i + 2] +
+               u_data[i + 3] * v_data[i + 3] + u_data[i + 4] * v_data[i + 4] + u_data[i + 5] * v_data[i + 5];
+    }
+
+    for (; i < length; ++i) {
         sum = sum + u_data[i] * v_data[i];
     }
 
@@ -94,15 +141,29 @@ int main() {
     start = __rdtsc();
     inner1(u, v, &dest);
     end = __rdtsc();
-    cpe = (double) (end - start) / length;
+    cpe = (double)(end - start) / length;
     printf("inner1 (baseline):          %5.2lf\n", cpe);
 
     // inner2 - 6x1 unrolling
     start = __rdtsc();
     inner2(u, v, &dest);
     end = __rdtsc();
-    cpe = (double) (end - start) / length;
+    cpe = (double)(end - start) / length;
     printf("inner2 (6x1 unrolling):     %5.2lf\n", cpe);
+
+    // inner3 - 6x6 unrolling
+    start = __rdtsc();
+    inner3(u, v, &dest);
+    end = __rdtsc();
+    cpe = (double)(end - start) / length;
+    printf("inner3 (6x6 unrolling):     %5.2lf\n", cpe);
+
+    // inner4 - 6x1a unrolling
+    start = __rdtsc();
+    inner4(u, v, &dest);
+    end = __rdtsc();
+    cpe = (double)(end - start) / length;
+    printf("inner3 (6x1a unrolling):    %5.2lf\n", cpe);
 
     return 0;
 }
